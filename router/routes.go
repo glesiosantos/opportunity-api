9ee9c8b605b1ecd2afb4f9676eb94d5f367ade4e
Glêsio Santos <glesioss@gmail.com>
2026-09-24 12:02:55 -0300

    configurando rotas para openings

--- router/routes.go
+++ router/routes.go
@@ -1,5 +1,34 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func initializeRoutes(router *gin.Engine) {}
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Get Opening",
+			})
+		})
+		v1.POST("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Post Opening",
+			})
+		})
+		v1.DELETE("/openings/{id}", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Delete Opening",
+			})
+		})
+		v1.PUT("/openings/edit/{id}", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Put Opening",
+			})
+		})
+
+	}
+}
